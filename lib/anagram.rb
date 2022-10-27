7686bc7ae5aa5fab51b1d2d1f3bd81ef842671c6
Pronepoet <ochiengphillip84@gmail.com>
2022-10-27 03:37:33 +0300

    Completed Assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
 # Your code goes here!
+class Anagram
+    attr_accessor :customer
+    def initialize(word)
+        @customer = word
+    end
+
+    def match(array)
+        array.select{|x| x.split("").sort == @customer.split("").sort}
+    end
+end
